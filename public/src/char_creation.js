188c6d3f7aebf34bc8a8d5da13d8ce667d6a1c6d
Emily Rhee <emilyrhee99@gmail.com>
2022-09-06 14:35:37 -0400

    added another canvas

--- public/src/char_creation.js
+++ public/src/char_creation.js
@@ -1,6 +1,6 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-const canvas2 = document.querySelector('canvas');
+const canvas1 = document.getElementById('canvas1');
+const c1 = canvas1.getContext('2d');
+const canvas2 = document.getElementById('canvas2');
 const c2 = canvas2.getContext('2d');
 
 var nameInput = document.getElementById("name");
@@ -13,8 +13,8 @@ playerSkin.src = "images/player_skin.png";
 const body = {
   width: playerSkin.width,
   height: playerSkin.height,
-  x: canvas.width / 12,
-  y: canvas.height / 7
+  x: canvas1.width / 12,
+  y: canvas1.height / 7
 }
 
 const skin1 = new Path2D();
@@ -32,46 +32,40 @@ skin3.color = '#8a6344';
 skin3.x = skin1.x + 80;
 skin3.y = 200;
 
-canvas.addEventListener('click', (event) => {
-  if (c.isPointInPath(skin1, event.offsetX, event.offsetY))
+var currentSkin = skin1.color;
+
+canvas2.addEventListener('click', (event) => {
+  if (c2.isPointInPath(skin1, event.offsetX, event.offsetY))
     currentSkin = skin1.color;
 
-  if (c.isPointInPath(skin2, event.offsetX, event.offsetY))
+  if (c2.isPointInPath(skin2, event.offsetX, event.offsetY))
     currentSkin = skin2.color;
 
-  if (c.isPointInPath(skin3, event.offsetX, event.offsetY))
+  if (c2.isPointInPath(skin3, event.offsetX, event.offsetY))
     currentSkin = skin3.color;
 });
 
 function changeSkin(s) {
-  c.fillStyle = s;
-  c.globalCompositeOperation = "overlay";
-  c.fillRect(body.x, body.y, playerSkin.width, playerSkin.height);
+  c1.fillStyle = s;
+  c1.globalCompositeOperation = "overlay";
+  c1.fillRect(body.x, body.y, playerSkin.width, playerSkin.height);
   
-  c.globalCompositeOperation = 'destination-in';
-  c.drawImage(playerSkin, body.x, body.y);
-  c.globalCompositeOperation = "source-over";
-}
-
-function skinButton(obj) {
-  obj.rect(obj.x, obj.y, 20, 20);
-  c.fillStyle = obj.color;
-  c.fill(obj);
+  c1.globalCompositeOperation = 'destination-in';
+  c1.drawImage(playerSkin, body.x, body.y);
+  c1.globalCompositeOperation = "source-over";
 }
 
 currentSkin = skin1.color;
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c1.clearRect(0, 0, canvas1.width, canvas1.height);
+  c2.clearRect(0, 0, canvas2.width, canvas2.height);
 
-  c.drawImage(playerSkin, body.x, body.y, playerSkin.width, playerSkin.height);  
-  //c.drawImage(bangs, body.x, body.y, playerSkin.width, playerSkin.height); 
-
+  c1.drawImage(playerSkin, body.x, body.y, playerSkin.width, playerSkin.height);
   changeSkin(currentSkin);
 
-  skinButton(skin1);  // fix flickering
-  skinButton(skin2);
-  skinButton(skin3);
+  c2.drawImage(bangs, body.x, body.y, playerSkin.width, playerSkin.height); 
+
 }
 animate();
