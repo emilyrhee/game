d9241e5394e31ec3c6de3c62658ee37ad2cfa780
Emily Rhee <emilyrhee99@gmail.com>
2022-09-30 15:10:16 -0400

    drew player sprite to canvas; added Sprite class

--- public/src/main.js
+++ public/src/main.js
@@ -1,3 +1,23 @@
-const dungeon = document.getElementById('dungeon');
-const ctx = dungeon.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+class Sprite {
+  constructor(x, y, src) {
+    this.x = x;
+    this.y = y;
+    this.img = new Image();
+    this.img.src = "images/" + src + ".png";
+  }
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, 80, 80);
+  }
+}
+
+player = new Sprite(300, 300, "player");
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  player.draw();
+}
+animate();
