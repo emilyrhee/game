c98d24b9b53e564fb211968bdc3974b7c248d025
Emily Rhee <emilyrhee99@gmail.com>
2022-10-01 16:15:48 -0400

    added movement

--- public/src/main.js
+++ public/src/main.js
@@ -13,11 +13,38 @@ class Sprite {
   }
 }
 
+var keyMap = {};
+
+function keys(){
+  if (keyMap["w"]) {
+    player.y--;
+  }
+  if (keyMap["a"]) {
+    player.x--;
+  }
+  if (keyMap["s"]) {
+    player.y++;
+  } 
+  if (keyMap["d"]) {
+    player.x++;
+  }
+}
+
+document.addEventListener('keydown', (event) => {
+  keyMap[event.key] = event.type == 'keydown'; // true
+});
+
+document.addEventListener('keyup', (event) => {
+  keyMap[event.key] = event.type == 'keydown'; //false
+});
+
 player = new Sprite(300, 300, "player");
 
 function animate() {
   requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   player.draw();
+  keys();
 }
 animate();
